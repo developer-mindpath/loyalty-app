2eb6772bd48851b752f0b033e03b050bba991265
Krishan Kripal <developer@mindpathtech.com>
2023-06-28 15:47:49 +0530

    changes in get customer api

--- backend/src/services/loyaltyProgramActivityService.ts
+++ backend/src/services/loyaltyProgramActivityService.ts
@@ -1,6 +1,7 @@
 import { GetLoyaltyProgramActivityResponse } from "../types/response/activity/getLoyaltyProgramActivityResponse";
 import LoyaltyProgramActivityRepository from "../repository/loyaltyProgramActivityRepository";
 import PaginationDTO from "../dto/paginationDTO";
+import { PointsCustomer } from "../types/response/customer/getCustomerDetailsResponse";
 
 export default class LoyaltyProgramActivityService {
   private _loyaltyProgramActivityRepository: LoyaltyProgramActivityRepository;
@@ -21,9 +22,21 @@ export default class LoyaltyProgramActivityService {
 
   public async getCustomerPointDetails(
     customerId: number
-  ): Promise<Array<Record<string, string | number>>> {
-    return await this._loyaltyProgramActivityRepository.getCustomerPointDetails(
-      customerId
-    );
+  ): Promise<Array<PointsCustomer>> {
+    const pointRecords =
+      await this._loyaltyProgramActivityRepository.getCustomerPointDetails(
+        customerId
+      );
+    const pointCustomer = new Array<PointsCustomer>();
+    for (let index = 0; index < pointRecords.length; index++) {
+      const pointRecord = pointRecords[index];
+      const pointCustomerRecord = {
+        action: pointRecord.action ? pointRecord.action : "",
+        points: pointRecord.points ? Number(pointRecord.points) : 0,
+        date: new Date(pointRecord.date),
+      } as PointsCustomer;
+      pointCustomer.push(pointCustomerRecord);
+    }
+    return pointCustomer;
   }
 }
